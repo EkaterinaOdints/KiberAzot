d149951daa762d5d3d483ac364ad4671b23dd998
EkaterinaOdints <157913603+EkaterinaOdints@users.noreply.github.com>
2025-11-19 17:12:12 +0300

    add Header

--- src/main.js
+++ src/main.js
@@ -1,3 +1,39 @@
-import './styles/styles.scss';
+import "./styles/styles.scss";
 
-console.log('Vite работает!');
+const useMobileMenu = () => {
+  const body = document.body;
+  const header = body.querySelector("[data-js-header]");
+  const menuButton = header.querySelector("[data-js-menu-button]");
+
+  if (header && menuButton) {
+    let isMenuOpened = false;
+
+    const openMenu = () => {
+      header.classList.add("is-menu-opened");
+      body.classList.add("overlay");
+      isMenuOpened = true;
+    };
+
+    const closeMenu = () => {
+      header.classList.remove("is-menu-opened");
+      body.classList.remove("overlay");
+      isMenuOpened = false;
+    };
+
+    menuButton.addEventListener("click", () => {
+      if (isMenuOpened) {
+        closeMenu();
+      } else {
+        openMenu();
+      }
+    });
+
+    window.addEventListener("resize", () => {
+      if (isMenuOpened) {
+        closeMenu();
+      }
+    });
+  }
+};
+
+useMobileMenu();
